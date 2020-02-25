686cb3d8dac5e4105d133575b9093994e71c3f84
jian chen <2899340@qq.com>
2020-02-25 15:34:31 +0800

    添加一个以线程方式导入动态库模态对话框的类。

--- examples/MFCApplication1/CDirSelectThread.cpp
+++ examples/MFCApplication1/CDirSelectThread.cpp
@@ -1,8 +1,5 @@
 #include "pch.h"
 #include "CDirSelectThread.h"
-#include "D:\\works\\cppTest\\cppTest\\MFCLibrary1\\MyDialog.h"
-#include "D:\\works\\cppTest\cppTest\MFCLibrary1\resource.h"
-#pragma comment(lib ,"D:\\works\\NimDuilibFramework\\bin\\MFCLibrary1.lib")
 
 IMPLEMENT_DYNCREATE(CDirSelectThread, CWinThread)
 
@@ -29,14 +26,10 @@ BOOL CDirSelectThread::InitInstance()
 
 int CDirSelectThread::Run()
 {
-    MessageBox(NULL, L"fuck", L"a", 1);
     auto hDll = LoadLibrary(_T("D:\\works\\NimDuilibFramework\\bin\\MFCLibrary1.dll"));// 加载DLL库文件，DLL名称和路径用自己的
     typedef void (*pExport)(void);
     pExport f= (pExport)GetProcAddress(hDll, "fuck");
     f();
-    fuck();
-    for (int i = 0; i != 10000; ++i)
-        OutputDebugString(L"fuckmom\n");
     Notify();
     return 1;
 }
