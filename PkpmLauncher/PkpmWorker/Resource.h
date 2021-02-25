55f454a32473b10f2186252b0442eb01aa57a511
jian chen <2899340@qq.com>
2021-02-25 17:38:06 +0800

    配置release编译环境

--- PkpmLauncher/PkpmWorker/Resource.h
+++ PkpmLauncher/PkpmWorker/Resource.h
@@ -4,15 +4,8 @@
 //
 #define IDC_MYICON                      2
 #define IDD_PKPMWORKER_DIALOG           102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
-#define IDM_ABOUT                       104
-#define IDM_EXIT                        105
 #define IDI_PKPMWORKER                  107
-#define IDI_SMALL                       108
-#define IDC_PKPMWORKER                  109
 #define IDR_MAINFRAME                   128
-#define IDI_ICONV51                     130
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
